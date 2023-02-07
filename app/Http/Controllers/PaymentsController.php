1a05f7a9a725288efdabe392a4127fc524e1370c
SebastianLerch <sebastian.lerch@adyen.com>
2023-02-07 08:00:25 +0000

    implemented first draft of paymentsController

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PaymentsController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function store()
+    {
+        $response = Http::accept('application/json')
+        ->withOptions([
+            'proxy' => 'http://sandboxproxy-vip.system.osext1.nlzwo1o.adyen.cloud:3128'
+         ])
+         ->withHeaders([
+            'X-API-Key' => env('ADYEN_API_KEY', null),
+            'Content-Type' => 'application/json',
+        ])
+        ->post(env('ADYEN_PAYMENTS_ENDPOINT',null), [
+            'merchantAccount' => env('ADYEN_MERCHANT_ACCOUNT_NAME',null),
+            'amount' => [
+                'currency' => 'CHF',
+                'value' => 1000,
+            ],
+            'reference' => 'My test reference 123',
+            'paymentMethod' => request("paymentMethod"),
+            'returnUrl' => 'https://example.com',
+        ]);
+
+        return $response->json();
+    }
 }
--- resources/js/adyen/dropin.js
+++ resources/js/adyen/dropin.js
@@ -18,6 +18,25 @@ export async function dropin(paymentMethodsArray){
         onError: (error, component) => {
             console.error(error.name, error.message, error.stack, component);
         },
+        onSubmit(state, dropin){
+          console.log(state.data);
+          makePayment(state.data)
+          .then(response => {
+            if (response.action) {
+              // Drop-in handles the action object from the /payments response
+              dropin.handleAction(response.action);
+            } else {
+              // Your function to show the final result to the shopper
+              showFinalResult(response);
+            }
+          })
+          .catch(error => {
+            throw Error(error);
+          });
+        },        
+        onAdditionalData(state, dropin){
+          console.log(state.data);
+        },
         // Any payment method specific configuration. Find the configuration specific to each payment method:  https://docs.adyen.com/payment-methods
         // For example, this is 3D Secure configuration for cards:
         paymentMethodsConfiguration: {
@@ -30,5 +49,26 @@ export async function dropin(paymentMethodsArray){
       };
       
       return (await AdyenCheckout(configuration)).create('dropin').mount(document.getElementById('dropin-container'))
+}
 
+const httpPost = (endpoint, data) =>
+    fetch(`${endpoint}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.json());
+
+async function makePayment(data){
+  try {
+    const response = await httpPost("/api/payments", data);
+    console.log(response);
+    if (response.error)
+      throw "Payment initiation failed";
+    return response;
+  } catch (data_2) {
+    return console.error(data_2);
+  }
 }
