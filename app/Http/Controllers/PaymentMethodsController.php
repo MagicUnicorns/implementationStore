b39bbf82e84e621a277c21571ee5b261fb188f7f
SebastianLerch <sebastian.lerch@adyen.com>
2023-02-08 09:34:10 +0000

    fixed payment methods call

--- app/Http/Controllers/PaymentMethodsController.php
+++ app/Http/Controllers/PaymentMethodsController.php
@@ -34,6 +34,11 @@ class PaymentMethodsController extends Controller
         ])
         ->post(env('ADYEN_PAYMENT_METHODS_ENDPOINT',null), [
             'merchantAccount' => env('ADYEN_MERCHANT_ACCOUNT_NAME',null),
+            'amount' => [
+                'currency' => 'CHF',
+                'value' => '1000',
+            ],
+            'countryCode' => 'CH',
             'allowedPaymentMethods' => [
                 'ideal',
                 'giropay',
